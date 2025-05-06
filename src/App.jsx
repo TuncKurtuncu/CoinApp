476bffcc4a092cdcd2ccefec188028be70e2b0a9
TuncKurtuncu <tunckurtuncu@gmail.com>
2025-05-06 19:03:10 +0300

    config

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,8 @@
-
 import './App.css'
 import ProtectedRoute from './components/ProtectedRoute';
 import HomePage from './pages/Home';
 import LoginRegisterPage from './pages/LoginRegister';
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { HashRouter, Routes, Route } from 'react-router-dom';
 import TopGainers from './pages/TopGainers';
 import TopLosers from './pages/TopLosers';
 import Details from './pages/Details';
@@ -13,10 +12,6 @@ import Profile from './pages/Profile';
 import { onAuthStateChanged } from "firebase/auth";
 import { auth } from "./api/firebase";
 import { useEffect, useState } from "react";
-
-
-
-
 
 function App() {
   const [user, setUser] = useState(null);
@@ -30,25 +25,24 @@ function App() {
 
   return (
     <div className='bg-[#181A20] min-h-screen'> 
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<HomePage user={user}/>}/>
-        <Route path="/login" element={<LoginRegisterPage />} />
-        <Route path='/gainers' element={<TopGainers/>} />
-        <Route path='/losers' element={<TopLosers/>}/>
-        <Route path='/topvolume' element={<TopVolume/>}/>
-        <Route path='/newcoins' element={<NewCoins/>}/>
-        <Route path='/coin/:id' element={<Details/>}/>
-        <Route path="/profile" element={
-  <ProtectedRoute>
-    <Profile />
-  </ProtectedRoute>
-} />
-      </Routes>
-    </BrowserRouter>
-      
+      <HashRouter> {/* HashRouter kullanıldı */}
+        <Routes>
+          <Route path="/" element={<HomePage user={user}/>}/>
+          <Route path="/login" element={<LoginRegisterPage />} />
+          <Route path='/gainers' element={<TopGainers/>} />
+          <Route path='/losers' element={<TopLosers/>}/>
+          <Route path='/topvolume' element={<TopVolume/>}/>
+          <Route path='/newcoins' element={<NewCoins/>}/>
+          <Route path='/coin/:id' element={<Details/>}/>
+          <Route path="/profile" element={
+            <ProtectedRoute>
+              <Profile />
+            </ProtectedRoute>
+          } />
+        </Routes>
+      </HashRouter>
     </div>
   );
 }
 
-export default App
+export default App;
