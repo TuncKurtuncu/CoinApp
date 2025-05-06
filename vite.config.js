55e22bc8a7a0d07906ead7053f01e66e85d44976
TuncKurtuncu <tunckurtuncu@gmail.com>
2025-05-06 19:07:57 +0300

    config

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react'
 import tailwindcss from "@tailwindcss/vite";
 
 export default defineConfig({
-  base: '/',
+  base: '/coinApp/', // Uygulamanızın bulunduğu alt klasörü belirtin
   plugins: [
     react(),
     tailwindcss()
