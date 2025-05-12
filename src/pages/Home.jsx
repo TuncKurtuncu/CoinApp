a73e09395add454ab1a08350f6906cb87e462222
TuncKurtuncu <tunckurtuncu@gmail.com>
2025-05-12 05:12:15 +0300

    search bar update

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -3,30 +3,40 @@ import AllCoins from '../components/AllCoins'
 import Header from '../components/Header'
 import ImagesBar from '../components/ImagesBar'
 import TopMovers from '../components/TopMovers'
+import SearchBar from '../components/SearchBar'
 
 
 
 
 function HomePage() {
   return (
-    <div >
+    <main>
      
-        <div className=' sticky top-0 z-50'>
-        <Header/>
-      </div>
-      <div className='container m-auto'>
-        <TopMovers/>
-      </div>
+      <header className="sticky top-0 z-50">
+        <Header />
+      </header>
+
       
-      <div className='container m-auto px-10 mt-10 mb-5'>
-        <ImagesBar/>
+      <div className="container mx-auto">
+        <section aria-label="Top movers">
+          <TopMovers />
+        </section>
+
+        <section className="px-10 mt-10 mb-5" aria-label="Coin search">
+          <SearchBar />
+        </section>
+
+        <section className="px-10 mt-10 mb-5" aria-label="Promotional images">
+          <ImagesBar />
+        </section>
+
+        <section className="px-5 mt-9" aria-label="All coins list">
+          <AllCoins />
+        </section>
       </div>
-      <div className='container m-auto px-5 mt-9 '>
-        <AllCoins/>
-        
-      </div>
-     
-    </div>
+    </main>
+
+
   )
 }
 
